cb0e8b8e49e5ee8cd3adbe83d937aedde257dd90
Owen Jones <owen@owenjones.net>
2022-09-14 12:24:52 +0100

    only display joining message if leaderboard ID is set

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
 import "./Header.css"
 
 function Header(props) {
+  if(process.env.REACT_APP_LEADERBOARD_CODE) {
+    var leaderboard_signup = <p><strong>Sign up at <span className="url">adventofcode.com</span> and join the leaderboard using code <span className="code">{process.env.REACT_APP_LEADERBOARD_CODE}</span>!</strong></p>
+  }
+  
   return (
     <div className="Header">
       <h1>Advent of Code 2021</h1>
       <p>An Advent calender of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like.</p>
-      <p><strong>Sign up at <span className="url">adventofcode.com</span> and join the leaderboard using code <span className="code">{process.env.REACT_APP_LEADERBOARD_CODE}</span>!</strong></p>
+      {leaderboard_signup}
     </div>
   );
 }
