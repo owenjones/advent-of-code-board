23aeb09560e2c14a01f0718380cca1b8b48a5374
Owen Jones <owen@owenjones.net>
2022-10-31 12:14:23 +0000

    preparing for 2022 event

--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,13 @@ function Header(props) {
   if(process.env.REACT_APP_LEADERBOARD_CODE) {
     var leaderboard_signup = <p><strong>Sign up at <span className="url">adventofcode.com</span> and join the leaderboard using code <span className="code">{process.env.REACT_APP_LEADERBOARD_CODE}</span>!</strong></p>
   }
-  
+
   return (
     <div className="Header">
-      <h1>Advent of Code 2021</h1>
-      <p>An Advent calender of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like.</p>
-      {leaderboard_signup}
+      <h1>Advent of Code 2022</h1>
+      <p>An Advent calender of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like</p>
+      <h2>Starts 1<sup>st</sup> December</h2>
+      <p>Find out more and practice on previous years events at <span className="url">adventofcode.com</span></p>
     </div>
   );
 }
