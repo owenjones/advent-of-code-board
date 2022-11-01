a7fed906db3c31a7597fe7515ae74f4e81abfe5c
Owen Jones <owen@owenjones.net>
2022-11-01 14:54:12 +0000

    merged Header component into main App component as it was bulk of the content

--- board/src/App.js
+++ board/src/App.js
@@ -1,14 +1,36 @@
 import Tree from './Tree'
-import Header from './Header'
 // import Leaderboard from './Leaderboard';
 import './App.css';
 
 function App() {
+  var date = new Date();
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+
+  if(process.env.REACT_APP_LEADERBOARD_CODE) {
+    var signup = <p><strong>Sign up at <span className="url">adventofcode.com</span> and join the leaderboard using code <span className="code">{ process.env.REACT_APP_LEADERBOARD_CODE }</span>!</strong></p>
+  } else {
+    var signup = <p>Find out more and practice on previous years events at <span className="url">adventofcode.com</span>!</p>;
+  }
+
+  if(month < 12) {
+    var promo = (
+      <>
+        <h2>Starts 1<sup>st</sup> December</h2>
+        { signup }
+      </>
+    );
+  }
+
   return (
     <div className="App">
       <Tree />
       <div className="Body">
-        <Header />
+        <div className="Header">
+          <h1>Advent of Code { year }</h1>
+          <p><small>An Advent calender of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like</small></p>
+          { promo }
+        </div>
       </div>
     </div>
   );
