4f6ea2301688d2cd3fc4320d9d929d591b2490fc
Owen Jones <owen@owenjones.net>
2022-11-01 13:37:32 +0000

    API caching layer, will fetch a private leaderboard and cache it

---
+++ api/api.php
@@ -0,0 +1,44 @@
+<?php
+// Simple cache layer on the Advent of Code API to prevent calling it too frequently.
+
+// They request no more than once every 15 mins so we'll do once every 30 to start,
+// and only between working hours (8am - 6pm).
+
+ob_start();
+require 'vendor/autoload.php';
+
+use \Dotenv\Dotenv;
+use \GuzzleHttp\Cookie\CookieJar;
+use \GuzzleHttp\Client;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+if(file_exists($_ENV["CACHE"])) {
+  $cache = json_decode(file_get_contents($_ENV["CACHE"]), TRUE);
+} else {
+  $cache = array();
+}
+
+$now = time();
+$hour = (int) date("H");
+
+// only update from AOC if: we haven't before, or we have and it was more than 30 mins ago (and it's between 8am and 6pm)
+if(!isset($cache['time']) || (isset($cache['time']) && (($now - $cache['time']) > 1800) && ($hour >= 8) && ($hour <= 18))) {
+  $cookies = CookieJar::fromArray(['session' => $_ENV["SESSION"]], ".adventofcode.com");
+  $client = new Client();
+  $response = $client->request("GET", $_ENV["API"], ["cookies" => $cookies]);
+
+  if($response->getStatusCode() == 200) {
+    $cache["status"] = 200;
+    $cache["data"] = json_decode((string) $response->getBody());
+    $cache["time"] = $now;
+    file_put_contents($_ENV["CACHE"], json_encode($cache));
+  } else {
+    $cache["status"] = $response->getStatusCode();
+    $cache["error"] = $response->getBody();
+  }
+}
+
+header("Content-Type: application/json");
+print(json_encode($cache));
