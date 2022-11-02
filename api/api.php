b99c8f8409a2dbc81d4492e5a7730997227c3807
Owen Jones <owen@owenjones.net>
2022-11-02 14:30:23 +0000

    make new structure on successful return; decode data first so we can manipulate it

--- api/api.php
+++ api/api.php
@@ -1,8 +1,6 @@
 <?php
-// Simple cache layer on the Advent of Code API to prevent calling it too frequently.
-
-// They request no more than once every 15 mins so we'll do once every 30 to start,
-// and only between working hours (8am - 6pm).
+// Simple wrapper around the Advent of Code API to prevent calling it too frequently and to simplify
+// the data returned to the leaderboard display (and strip any user IDs etc. from the publicly available output).
 
 ob_start();
 require 'vendor/autoload.php';
@@ -24,7 +22,6 @@ if(file_exists($_ENV["CACHE"])) {
 $now = time();
 $hour = (int) date("H");
 
-
 // only update from AOC if: we haven't before, or we have and it was more than 30 mins ago (and it's between 8am and 6pm)
 if(!isset($cache['time']) || (isset($cache['time']) && (($now - $cache['time']) > 1800) && ($hour >= 8) && ($hour <= 18))) {
   $cookies = CookieJar::fromArray(['session' => $_ENV["SESSION"]], ".adventofcode.com");
@@ -36,9 +33,7 @@ if(!isset($cache['time']) || (isset($cache['time']) && (($now - $cache['time']) 
     $response = $client->request("GET", $url, ["cookies" => $cookies]);
 
     if($response->getStatusCode() == 200) {
-      $cache["status"] = 200;
-      $cache["data"] = json_decode((string) $response->getBody());
-      $cache["time"] = $now;
+      $data = json_decode((string) $response->getBody());
 
       // manipulate data here to simplify displaying?
       // some possible outputs:
@@ -46,6 +41,10 @@ if(!isset($cache['time']) || (isset($cache['time']) && (($now - $cache['time']) 
       // - for current day: ordered array of people completed
       // - 'event log' - last things completed and by who
 
+      $cache = array();
+      $cache["data"] = $data; // for now - pass entire data structure through
+      $cache["status"] = 200;
+      $cache["time"] = $now;
       file_put_contents($_ENV["CACHE"], json_encode($cache));
     } else {
       // if we got an unexpected response: return old cached data (if it exists) but also attach current error so it can eventually get picked up
