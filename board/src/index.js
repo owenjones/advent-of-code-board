4228110cba176ae3db7c34a4b1555063c32ddaff
Owen Jones <owen@owenjones.net>
2023-11-14 11:40:56 +0000

    render in StrictMode for development

--- board/src/index.js
+++ board/src/index.js
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 import './index.css';
 import App from './App';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
